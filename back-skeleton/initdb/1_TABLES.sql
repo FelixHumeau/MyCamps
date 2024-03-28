b342bf75ba43ca2fe9f879cb13d3808342a48e40
fefe <humeauf@yahoo.com>
2024-03-28 18:21:48 +0100

    implémentation table

--- back-skeleton/initdb/1_TABLES.sql
+++ back-skeleton/initdb/1_TABLES.sql
@@ -1,30 +1,88 @@
-create table students
+create table utilisateurs
 (
-    id SERIAL PRIMARY KEY,
-    first_name TEXT not null,
-    last_name TEXT not null,
-    birthdate date null,
-    major_id int null,
-    image bytea null
+    id_utilisateur INT primary key,
+    nom VARCHAR(250) not null,
+    prenom VARCHAR(250) not null,
+    adresse_mail VARCHAR(250) not null,
+    rôle varchar(250) not null,
+    mot_de_passe VARCHAR(250) not null
 );
 
-create table majors
+create table Fichiers_partagés
 (
-    id SERIAL PRIMARY KEY,
-    name TEXT not null,
-    description TEXT not null
+    id_fichier INT PRIMARY KEY,
+    titre VARCHAR(250) not null,
+    description TEXT not null,
+    chemin TEXT not null,
+    date_de_partage DATE not null,
+    id_utilisateur INT not null,
+    FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateurs(ID_Utilisateur)
 );
 
-create table courses
-(
-    id SERIAL PRIMARY KEY,
-    name TEXT not null,
-    hours int not null
+CREATE TABLE Matieres (
+    ID_matiere INT PRIMARY KEY,
+    Nom_matiere VARCHAR(255) NOT NULL,
+    Description TEXT
 );
 
-create table student_course
-(
-    id SERIAL PRIMARY KEY,
-    student_id int not null,
-    course_id int not null
+CREATE TABLE Classe (
+    ID_classe INT PRIMARY KEY,
+    Nom_classe VARCHAR(255) NOT NULL,
+    Description TEXT
 );
+
+CREATE TABLE Notes (
+    ID_note INT PRIMARY KEY,
+    ID_utilisateur INT,
+    ID_matiere INT,
+    Note DECIMAL(5,2),
+    Commentaires TEXT,
+    Date DATE NOT NULL,
+    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
+    FOREIGN KEY (ID_matiere) REFERENCES Matieres(ID_matiere)
+);
+
+CREATE TABLE Emplois_du_temps (
+    ID_emploi_du_temps INT PRIMARY KEY,
+    ID_matiere INT,
+    ID_utilisateur INT,
+    Heure_debut TIME NOT NULL,
+    Heure_fin TIME NOT NULL,
+    Jour_de_la_semaine VARCHAR(255) NOT NULL,
+    Salle VARCHAR(255),
+    FOREIGN KEY (ID_matiere) REFERENCES Matieres(ID_matiere),
+    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur)
+);
+
+
+CREATE TABLE Associations_Utilisateur_Matiere (
+    ID_utilisateur INT,
+    ID_matiere INT,
+    PRIMARY KEY (ID_utilisateur, ID_matiere),
+    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
+    FOREIGN KEY (ID_matiere) REFERENCES Matieres(ID_matiere)
+  );
+
+
+CREATE TABLE Associations_Utilisateur_classe (
+    ID_utilisateur INT,
+    ID_fichier INT,
+    Role varchar(255) NOT NULL,
+    PRIMARY KEY (ID_utilisateur, ID_fichier),
+    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
+    FOREIGN KEY (ID_fichier) REFERENCES Fichiers_partagés(ID_fichier)
+);
+
+CREATE TABLE Permissions_Fichiers (
+    ID_utilisateur INT,
+    ID_fichier INT,
+    Role varchar(255) NOT NULL,
+    PRIMARY KEY (ID_utilisateur, ID_fichier),
+    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
+    FOREIGN KEY (ID_fichier) REFERENCES Fichiers_partagés(ID_fichier)
+
+);
+
+
+
+
